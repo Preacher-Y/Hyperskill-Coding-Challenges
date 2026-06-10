01e627c5311409e685b6175d7c92aa59202a82e9
Preacher-Y <ysheja@gmail.com>
2026-06-10 09:20:35 +0200

    Complete Pattern matching in practice

--- Topics/Runtime type checking/Pattern matching in practice/src/Main.java
+++ Topics/Runtime type checking/Pattern matching in practice/src/Main.java
@@ -5,6 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Object msg = scanner.nextLine();    
 
+        if (msg instanceof String str){
+            if (!str.trim().isEmpty()){
+                System.out.println("The variable is not empty, the length is "+str.length());
+            }
+        }
+
         // Write your code here
     }
 }
