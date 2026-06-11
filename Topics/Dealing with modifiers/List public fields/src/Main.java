cb18d9abf0abfc93271e3deda1f21cdefa546949
Preacher-Y <ysheja@gmail.com>
2026-06-11 13:30:17 +0200

    Done implementing Dealing with modifiers

--- Topics/Dealing with modifiers/List public fields/src/Main.java
+++ Topics/Dealing with modifiers/List public fields/src/Main.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 /**
  Get list of public fields the object declares (inherited fields should be skipped).
  */
@@ -5,6 +9,10 @@ class FieldGetter {
 
     public String[] getPublicFields(Object object) {
         // Add implementation here
+        return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(field -> Modifier.isPublic(field.getModifiers()))
+                .map(Field::getName)
+                .toArray(String[]::new);
     }
 
 }
