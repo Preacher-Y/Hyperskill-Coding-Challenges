2bc9d98dd38e8320e16f43c82e5eed8bff9ed530
Preacher-Y <ysheja@gmail.com>
2026-06-11 14:58:16 +0200

    Done with the invoking a method

--- Topics/Manipulating fields and methods/Invoking a method/src/Main.java
+++ Topics/Manipulating fields and methods/Invoking a method/src/Main.java
@@ -3,7 +3,15 @@ import java.lang.reflect.Method;
 
 class MethodsDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         // write your code here
+        SomeClass obj = new SomeClass();
+
+        Method method = SomeClass.class.getDeclaredMethods()[0];
+        method.setAccessible(true);
+
+        Object result = method.invoke(obj);
+
+        System.out.println(result);
     }
 }
